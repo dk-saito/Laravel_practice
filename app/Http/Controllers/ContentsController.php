8b4f6a324cc5d71e079983f47ad629a53fd02ecf
dk-saito <dk-saito@usen-next.jp>
2022-08-26 16:05:08 +0900

    database更新・CRUDのコントローラー仮完成

--- app/Http/Controllers/ContentsController.php
+++ app/Http/Controllers/ContentsController.php
@@ -20,7 +20,7 @@ class ContentsController extends Controller
         // $request->validate([
             // 'image' => 'required|max:1024|mimes:jpg,jpeg,png,gif'
             // ]);
-        // $file_path = $request->video->store('videos', 'public');
+        $file_path = $request->video->store('videos', 'public');
         // print("<img src='". asset("$file_path"). "' width='300'>");
         // print("<a href='upload_form'>画像投稿フォームに戻る</a>");
 
@@ -32,70 +32,69 @@ class ContentsController extends Controller
         return view('admin.list');
     }
 
-// ******************************************************************
-// ここまで完成↑
-// ******************************************************************
 
     // コンテンツ削除(DELETE)
-    public function delete_confirm($id, Request $request){
-        $task_to_delete = Task::find($id);
+    public function delete($id, Request $request){
+        $contents_to_delete = Content::find($id);
 
         if ($request->has('back')){
-            return redirect('/taskmanagementapp/list');
+            return redirect('/admin/list');
         }
 
         if($request->has('delete')){
-            $task_to_delete->delete();
-            return redirect('/taskmanagementapp/list');
+            $contents_to_delete->delete();
+            return redirect('/admin/list');
         }
 
-        return view('task_management_app.delete_confirm', compact('task_to_delete'));
+        return view('admin.delete', compact('contents_to_delete'));
     }
 
 // メッセージ編集(UPDATE)
-    public function edit($id, Request $request){
-        $task = Task::find($id);
-        // if($request->has('back')){
-        //     return redirect('/messageboard/index');
-        // }
+public function edit($id, Request $request){
+    $task = Content::find($id);
 
-        if($request->has('back')){
-            return redirect('/taskmanagementapp/list');
-        }
-        if($request->has('update')){
-            $this->validate($request, [
-                'task_detail' => ['required'],
-            'name' => ['required'],
-            'datetime_start' => ['required'],
-            'datetime_finish' => ['required']
-            ]);
-            $task = Task::find($id);
-            $task->task_detail = $request->task_detail;
-            $task->name = $request->name;
-            $task->datetime_start = $request->datetime_start;
-            $task->datetime_finish = $request->datetime_finish;
-            $task->save();
-            return redirect('/taskmanagementapp/list');
-        }
-
-        return view('task_management_app.edit', compact('task'));
+    if($request->has('back')){
+        return redirect('/admin/list');
     }
 
-    // ステータス変更
-    public function change_status($id, Request $request){
-        $tasks = Task::all();
-        $task = Task::find($id);
+    if($request->has('update')){
+        // $this->validate($request, [
+            //     'task_detail' => ['required'],
+            // 'name' => ['required'],
+            // 'datetime_start' => ['required'],
+            // 'datetime_finish' => ['required']
+            // ]);
+            $file_path = $request->video->store('videos', 'public');
 
-        if($task->status == 1){
-            $task->status = 0;
-            $task->save();
-            return redirect('/taskmanagementapp/list')->withInput();
-        }else{
-            $task->status = 1;
-            $task->save();
-            return redirect('/taskmanagementapp/list')->withInput();
+            $update_content = Content::find($id);
+            $update_content->name = $request->video->name;
+            $update_content->url = $file_path;
+            $update_content->save();
+            return redirect('/admin/list');
         }
 
-        return view('task_management_app.list', compact('task', 'tasks'));
+        return view('admin.edit', compact('update_content'));
     }
+
+    // ******************************************************************
+    // ここまで完成↑
+    // ******************************************************************
+
+    // 有料無料変更
+    // public function change_status($id, Request $request){
+        //     $tasks = Task::all();
+        //     $task = Task::find($id);
+
+        //     if($task->status == 1){
+            //         $task->status = 0;
+    //         $task->save();
+    //         return redirect('/taskmanagementapp/list')->withInput();
+    //     }else{
+    //         $task->status = 1;
+    //         $task->save();
+    //         return redirect('/taskmanagementapp/list')->withInput();
+    //     }
+
+    //     return view('task_management_app.list', compact('task', 'tasks'));
+    // }
 }
