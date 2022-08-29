d543c54908b2d3d380d4c07d4f110ca7e223b6d0
19980303 <ay-kawahara@usen-next.jp>
2022-08-29 15:35:15 +0900

    h

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -3,6 +3,40 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+
+        <style>.btn-brackets {
+            display: inline-block;
+            position: relative;
+            padding: 0.5em 1em;
+            text-decoration: none;
+            color: #000;
+            transition: .4s;
+          }
+          .btn-brackets:hover {
+            color:#ff7f7f;
+          }
+          .btn-brackets:before, .btn-brackets:after {
+            position: absolute;
+            top: 0;
+            content:'';
+            width: 8px;
+            height: 100%;
+            display: inline-block;
+          }
+          .btn-brackets:before {
+            border-left: solid 1px #ff7f7f;
+            border-top: solid 1px #ff7f7f;
+            border-bottom: solid 1px #ff7f7f;
+            left: 0;
+          }
+          .btn-brackets:after {
+            content: '';
+            border-top: solid 1px #ff7f7f;
+            border-right: solid 1px #ff7f7f;
+            border-bottom: solid 1px #ff7f7f;
+            right: 0;
+          }</style>
+
     </x-slot>
 
     <style></style>
@@ -12,7 +46,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h1>動画一覧</h1>
-                    <a href="./upload_form">動画アップロード</a>
+                    <a href="./upload_form" class="btn-brackets">
+                        動画アップロード
+                      </a>
                     <table border='1'>
                         <tr>
                             <th>ファイル名</th>
@@ -23,7 +59,7 @@
                         @foreach ($contents as $content)
                             <tr>
                                 <td>{{$content->name}}</td>
-                                <td><video src="{{asset($content->url)}}" width='200'></td>
+                                <td><video src="{{asset($content->url)}}" controls width='200'></video></td>
                                 <td>
                                     {{$content->url}}
                                     {{-- <form action=""> --}}
@@ -38,10 +74,11 @@
                                 </td>
                                 {{-- <td>http://localhost/{{$content->filepath}}</td> --}}
                                 <td>
-                                    <button onclick="location.href='/admin/delete/{{$content->id}}'">削除</button>
+                                    <button class="btn-brackets" onclick="location.href='/admin/delete/{{$content->id}}'">削除</button>
+
                                 </td>
                                 <td>
-                                    <button onclick="location.href='/admin/edit/{{$content->id}}'">編集</button>
+                                    <button class="btn-brackets" onclick="location.href='/admin/edit/{{$content->id}}'">編集</button>
                                 </td>
                             </tr>
                         @endforeach
